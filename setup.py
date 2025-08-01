80442a065d8c1ba8bb3646f0f7743b05c6033122
Ghada <ghadanoor.el@gmail.com>
2025-08-01 17:12:06 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="aqi-predictor",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+    ],
+)
